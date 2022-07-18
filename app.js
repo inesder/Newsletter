3bb58580d18a81c07bd6884aeeaa9d373b2cf5b7
Inès Derraz <inesderraz@hotmail.fr>
2022-07-18 12:00:22 +0200

    second commit

--- app.js
+++ app.js
@@ -53,10 +53,13 @@ app.post("failure", function(req, res) {
 });
 
 
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
-
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("Server is running on port 3000");
 });
 
